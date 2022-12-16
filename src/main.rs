1bd35cbac2ce1fde568cd4aa7f47e310d8eb01e4
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-16 12:17:53 -0600

    Progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
     // Image
     let image_width = 256;
@@ -7,7 +10,11 @@ fn main() {
     println!("P3\n{} {}\n255\n", image_width, image_height);
 
     for j in (0..image_height-1).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        io::stderr().flush().unwrap();
+
         for i in 0..image_width {
+            // Classic graphics gradient
             let r: f64 = (i as f64) / (image_width-1) as f64;
             let g: f64 = (j as f64) / (image_height-1) as f64;
             let b: f64 = 0.25;
@@ -19,4 +26,7 @@ fn main() {
             println!("{} {} {}\n", ir, ig, ib)
         }
     }
+
+    // Make it so progress indicator doesn't end up before terminal prompt
+    eprintln!()
 }
