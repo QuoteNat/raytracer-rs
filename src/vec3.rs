c699722ca9370840856eb73d5b34b7fe496838e3
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-16 13:48:35 -0600

    vec3 Utility functions

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,11 +1,12 @@
-use std::ops::{Neg, Index, IndexMut, AddAssign, MulAssign, DivAssign};
+use std::ops::{Neg, Index, IndexMut, AddAssign, MulAssign, DivAssign, Mul, Add, Div, Sub};
 
 /// A 3d vector struct
-pub struct vec3 {
+#[derive(Copy, Clone)]
+pub struct Vec3 {
     pub e: [f64; 3]
 }
 
-impl vec3 {
+impl Vec3 {
     /// Returns x value of the vector
     pub fn x(&self) -> f64 {
         return self.e[0]
@@ -30,10 +31,11 @@ impl vec3 {
     pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
+
 }
 
 /// Negation. Equivalent to -1 * vector
-impl Neg for vec3 {
+impl Neg for Vec3 {
     type Output = [f64; 3];
 
     fn neg(self) -> Self::Output {
@@ -42,7 +44,7 @@ impl Neg for vec3 {
 }
 
 /// Immutable element access operator
-impl Index<usize> for vec3 {
+impl Index<usize> for Vec3 {
     type Output = f64;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -51,13 +53,13 @@ impl Index<usize> for vec3 {
 }
 
 /// Mutable element access operator
-impl IndexMut<usize> for vec3 {
+impl IndexMut<usize> for Vec3 {
     fn index_mut(&mut self, index: usize) -> &mut f64 {
         return &mut self.e[index]
     }
 }
 
-impl AddAssign for vec3 {
+impl AddAssign for Vec3 {
     fn add_assign(&mut self, other: Self) {
         *self = Self {
             e: [self.x() + other.x(), self.y() + other.y(), self.z() + other.z()]
@@ -65,7 +67,7 @@ impl AddAssign for vec3 {
     }
 }
 
-impl MulAssign<f64> for vec3 {
+impl MulAssign<f64> for Vec3 {
     fn mul_assign(&mut self, other: f64) {
         *self = Self {
             e: [self.x() + other, self.y() + other, self.z() + other]
@@ -73,8 +75,92 @@ impl MulAssign<f64> for vec3 {
     }
 }
 
-impl DivAssign<f64> for vec3 {
+impl DivAssign<f64> for Vec3 {
     fn div_assign(&mut self, other: f64) {
         *self *= 1.0 / other;
     }
+}
+
+// Type aliases
+type Point3 = Vec3;
+type Color = Vec3;
+
+// Utlity functions!
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            e: [self.e[0] + other.e[0], self.e[1] + other.e[1], self.e[2] + other.e[2]]
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            e: [self.e[0] - other.e[0], self.e[1] - other.e[1], self.e[2] - other.e[2]]
+        }
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            e: [self.e[0] * other.e[0], self.e[1] * other.e[1], self.e[2] * other.e[2]]
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Self {
+            e: [self.e[0] * other, self.e[1] * other, self.e[2] * other]
+        }
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul (self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [other.e[0] * self, other.e[1] * self, other.e[2] * self]
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div (self, other: f64) -> Self::Output {
+        (1.0/other) * self
+    }
+}
+
+/// Dot product of two vectors
+pub fn dot(u: &Vec3, v: &Vec3) -> f64{
+    u.e[0] * v.e[0]
+    + u.e[1] * v.e[1]
+    + u.e[2] * v.e[2]
+}
+
+/// Cross product of two vectors
+pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+    Vec3 {
+        e: [u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]]
+    }
+}
+
+/// Unit vector
+pub fn unit_vector(v: Vec3) -> Vec3 {
+    v / v.length()
 }
