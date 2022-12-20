6b0f004f8f7b051313dea521c4eca79ecb0d4743
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-20 15:42:49 -0600

    Random number generation

--- src/utility.rs
+++ src/utility.rs
@@ -1,9 +1,23 @@
+extern crate rand;
+use rand::thread_rng;
+use rand::Rng;
+
+/// Max value of f64
 pub const INFINITY: f64 = f64::INFINITY;
-#[allow(dead_code)]
 pub const PI: f64 = 3.1415926535897932385;
 
-#[allow(dead_code)]
+/// Converts degrees to radians
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     return degrees * PI / 180.0;
 }
 
+/// Generates a random float in the range [min, max]
+pub fn random_float(min: f64, max: f64) -> f64 {
+    let mut rng = thread_rng();
+    return rng.gen_range(min..max)
+}
+
+pub fn random_float_1() -> f64 {
+    let mut rng = thread_rng();
+    return rng.gen_range(0.0..1.0)
+}
