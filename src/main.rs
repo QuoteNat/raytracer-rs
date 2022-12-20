da96ba7e530d20abf722fb5edb7abcb7add133dc
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-20 09:52:33 -0600

    Sphere normals

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,25 @@ use color::*;
 mod ray;
 use ray::*;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin - *center;
     let a = dot(&r.direction, &r.direction);
     let b = 2.0 * dot(&oc, &r.direction);
     let c = dot(&oc, &oc) - radius*radius;
     let discriminant = b*b - 4.0*a*c;
-    return discriminant > 0.0;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 /// Return ray color
 fn ray_color(r: &Ray) -> Color {
-    if hit_sphere(&Point3{e:[0.0, 0.0, -1.0]}, 0.5, r) {
-        return Color{e:[1.0, 0.0, 0.0]}
+    let t = hit_sphere(&Point3{e:[0.0, 0.0, -1.0]}, 0.5, r);
+    if t > 0.0 {
+        let N = unit_vector(r.at(t) - Vec3{e:[0.0, 0.0, -1.0]});
+        return 0.5 * Color{e:[N.x() + 1.0, N.y() + 1.0, N.z() + 1.0]};
     }
     let unit_direction = unit_vector(r.direction);
     let t = 0.5*(unit_direction.y() + 1.0);
