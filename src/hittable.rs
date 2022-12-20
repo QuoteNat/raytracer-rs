ff828b6671be494fee4d16ac9f68a3abe2c2387f
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-20 10:34:34 -0600

    set_face_normal for hit_record

--- src/hittable.rs
+++ src/hittable.rs
@@ -7,6 +7,18 @@ struct hit_record {
     t: f64,
 }
 
+impl hit_record {
+    fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        let front_face = dot(r.direction(), outward_normal) < 0;
+        if front_face {
+            self.normal = outward_normal;
+        } else {
+            self.normal = -outward_normal;
+        }
+    }
+}
+
+/// An object that can be intersected by a ray
 trait Hittable {
     /// Implements ray intersect function for a given object
     fn hit(r: &Ray, t_min: f64, t_max: f64, rec: &mut Rec) -> bool;
--- src/sphere.rs
+++ src/sphere.rs
@@ -32,7 +32,8 @@ impl Hittable for Sphere {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        let outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
 
         return true;
     }
