125c9ebdf100f9a8a652e0c9c8066cfd57cf4dba
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2022-12-20 16:09:23 -0600

    CameraCreator helper function

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,8 @@
-use vector::*;
-use create::ray::Ray;
+use crate::vector::*;
+use crate::ray::Ray;
 
-struct Camera {
+#[derive(Copy, Clone)]
+pub struct Camera {
     origin: Point3,
     horizontal: Vec3,
     vertical: Vec3,
@@ -9,22 +10,30 @@ struct Camera {
 }
 
 impl Camera {
-    fn Camera(&mut self) {
-        let aspect_ratio = 16.0 / 9.0;
-        let viewport_height = 2.0;
-        let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
-
-        self.origin = Point3{e:[0.0, 0.0, 0.0]};
-        self.horizontal = Point3{e:[viewport_width, 0.0, 0.0]};
-        self.vertical = Point3{e:[0.0, viewport_height, 0.0]};
-        self.lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3{e:[0.0, 0.0, focal_length]};
-    }
-
-    fn get_ray(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         return Ray {
             origin: self.origin,
             direction: self.lower_left_corner + u*self.horizontal + v*self.vertical - self.origin,
         }
+    }
+}
+
+/// Creates a new camera struct
+pub fn CameraCreator() -> Camera {
+    let aspect_ratio = 16.0 / 9.0;
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3{e:[0.0, 0.0, 0.0]};
+    let horizontal = Point3{e:[viewport_width, 0.0, 0.0]};
+    let vertical = Point3{e:[0.0, viewport_height, 0.0]};
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3{e:[0.0, 0.0, focal_length]};
+
+    Camera {
+        origin,
+        horizontal,
+        vertical,
+        lower_left_corner
     }
 }
