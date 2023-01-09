dae3cfae99c3f4e66e05b09dea962c53b199b4e7
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-01-09 11:52:35 -0600

    Changed buffer to vec of f64 values

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,31 +1,32 @@
 /// Buffer struct for storing image color data
 pub struct Buffer {
-    e: [i32],
-    width: i32,
-    height: i32,
+    e: Vec<f64>,
+    width: u32,
+    height: u32,
 }
 
 impl Buffer {
-    pub fn new(width: i32, height: i32) -> Buffer {
+    pub fn new(width: u32, height: u32) -> Buffer {
         Buffer {
-            e: [i32; width * height],
+            e: vec![0.0; (width * height) as usize],
             width,
             height,
         }
     }
 
     /// Returns the 1d array index of [r(ow), c(olumn)]
-    fn index(&self, r: i32, c: i32) -> i32 {
-        return r * height + c;
+    fn index(&self, r: u32, c: u32) -> usize {
+        return (r * self.height + c) as usize;
     }
 
     /// Change value at [x, y] to value
-    pub fn write(&mut self, value: i32, x: i32, y: i32) {
-        self.e[index(y, x)] = value;
+    pub fn write(&mut self, value: f64, x: u32, y: u32) {
+        let index = self.index(y, x);
+        self.e[index] = value;
     }
 
     /// Returns value at [x, y]
-    pub fn at(&self, x: i32, y: i32) -> i32 {
-        self.e[index(y, x)]
+    pub fn at(&self, x: u32, y: u32) -> f64 {
+        self.e[self.index(y, x)]
     }
 }
