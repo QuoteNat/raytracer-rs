fae1ec211ed55f67960be35402b6aa09be5b608f
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-01-23 15:43:54 -0600

    perlin smoothing

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,4 +1,7 @@
-use crate::{utility::{random_float_1, random_int}, vector::Point3};
+use crate::{
+    utility::{random_float, random_float_1, random_int},
+    vector::Point3,
+};
 
 pub struct Perlin {
     point_count: usize,
@@ -26,16 +29,53 @@ impl Perlin {
             ranfloat,
             perm_x,
             perm_y,
-            perm_z
+            perm_z,
         }
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        let u = p.x() - f64::floor(p.x());
+        let v = p.y() - f64::floor(p.y());
+        let w = p.z() - f64::floor(p.z());
+        let u = u * u * (3.0 - 2.0 * u);
+        let v = v * v * (3.0 - 2.0 * v);
+        let w = w * w * (3.0 - 2.0 * w);
 
-        return self.ranfloat[self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize]];
+        let i = f64::floor(p.x()) as i32;
+        let j = f64::floor(p.y()) as i32;
+        let k = f64::floor(p.z()) as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]];
+                }
+            }
+        }
+
+        return Perlin::trilinear_interp(&c, u, v, w);
+    }
+
+    fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    let fi = i as f64;
+                    let fj = j as f64;
+                    let fk = k as f64;
+                    accum += (fi * u + (1.0 - fi) * (1.0 - u))
+                        * (fj * v + (1.0 - fj) * (1.0 - v))
+                        * (fk * w + (1.0 - fk) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+
+        return accum;
     }
 
     fn perlin_generate_perm(point_count: usize) -> Vec<usize> {
