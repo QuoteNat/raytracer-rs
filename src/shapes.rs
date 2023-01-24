9ff5b4ee2b84dda0764d1a1ac75c4cc30d0c4d97
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-01-24 13:35:22 -0600

    Axis aligned rectangle

--- src/shapes.rs
+++ src/shapes.rs
@@ -164,3 +164,62 @@ impl Hittable for Triangle {
         AABB::new_from_points(&vec![self.point1, self.point2, self.point3])
     }
 }
+
+pub struct XYRect {
+    material: Rc<dyn Material>,
+    x0: f64,
+    x1: f64,
+    y0: f64,
+    y1: f64,
+    k: f64,
+}
+
+impl XYRect {
+    pub fn new(x0: f64, x1: f64, y0: f64, y1: f64, k: f64, material: &Rc<dyn Material>) -> XYRect {
+        XYRect {
+            material: Rc::clone(material),
+            x0,
+            x1,
+            y0,
+            y1,
+            k,
+        }
+    }
+}
+
+impl Hittable for XYRect {
+    fn bounding_box(&self) -> AABB {
+        AABB::new(
+            Vec3::new(self.x0, self.y0, self.k - 0.0001),
+            Vec3::new(self.x1, self.y1, self.k + 0.0001),
+        )
+    }
+
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let t = (self.k - r.origin.z()) / r.direction.z();
+        if t < t_min || t > t_max {
+            return None;
+        }
+
+        let x = r.origin.x() + t * r.direction.x();
+        let y = r.origin.y() + t * r.direction.y();
+        if x < self.x0 || x > self.x1 || y < self.y0 || y > self.y1 {
+            return None;
+        }
+        let mut rec = HitRecord {
+            p: r.at(t),
+            normal: Vec3::new(0.0, 0.0, 1.0),
+            material: Rc::clone(&self.material),
+            t,
+            front_face: true,
+            uv: TextureCoord::new(
+                (x - self.x0) / (self.x1 - self.x0),
+                (y - self.y0) / (self.y1 - self.y0),
+            ),
+        };
+        let normal = rec.normal;
+        rec.set_face_normal(r, &normal);
+
+        return Some(rec);
+    }
+}
