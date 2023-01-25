4a99e9269aa3f27fbe8924ef23a72cfb0c84bc40
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-01-25 13:16:03 -0600

    Clean up

--- src/scenes.rs
+++ src/scenes.rs
@@ -171,71 +171,6 @@ use crate::volumes::ConstantMedium;
 //     )
 // }
 
-// pub fn make_bubble() -> HittableList {
-//     // World
-//     let mut world = HittableList {
-//         objects: Vec::new(),
-//     };
-//     // let absorbance = 0.5;
-//     let material_ground: Rc<dyn Material> = Rc::new(Lambertian::new(quick_vec(1.0, 1.0, 1.0)));
-//     let material_center: Rc<dyn Material> = Rc::new(BlinnPhong::new(
-//         quick_vec(0.1, 0.2, 0.5),
-//         quick_vec(1.0, 1.0, 1.0),
-//         10.0,
-//     ));
-//     let material_left: Rc<dyn Material> = Rc::new(Dielectric { ir: 1.5 });
-//     let material_right: Rc<dyn Material> = Rc::new(Metal {
-//         albedo: quick_vec(0.8, 0.6, 0.2),
-//         fuzz: 0.0,
-//     });
-
-//     let material_triangle: Rc<dyn Material> = Rc::new(Metal {
-//         albedo: quick_vec(1.0, 0.5, 0.5),
-//         fuzz: 0.0,
-//     });
-
-//     world.add(Rc::new(Triangle {
-//         point1: quick_vec(2.0, 0.0, -2.0),
-//         point2: quick_vec(-2.0, 0.0, -2.0),
-//         point3: quick_vec(0.0, 1.5, -0.5),
-//         material: Rc::clone(&material_triangle),
-//     }));
-
-//     let sphere_ground: Rc<dyn Hittable> = Rc::new(Sphere {
-//         center: quick_vec(0.0, -100.5, -1.0),
-//         radius: 100.0,
-//         material: Rc::clone(&material_ground),
-//     });
-//     let sphere_center: Rc<dyn Hittable> = Rc::new(Sphere {
-//         center: quick_vec(0.0, 0.0, -1.0),
-//         radius: 0.5,
-//         material: Rc::clone(&material_center),
-//     });
-//     let sphere_left: Rc<dyn Hittable> = Rc::new(Sphere {
-//         center: quick_vec(-1.0, 0.0, -1.0),
-//         radius: 0.5,
-//         material: Rc::clone(&material_left),
-//     });
-//     let sphere_bubble: Rc<dyn Hittable> = Rc::new(Sphere {
-//         center: quick_vec(-1.0, 0.0, -1.0),
-//         radius: -0.4,
-//         material: Rc::clone(&material_left),
-//     });
-//     let sphere_right: Rc<dyn Hittable> = Rc::new(Sphere {
-//         center: quick_vec(1.0, 0.0, -1.0),
-//         radius: 0.5,
-//         material: Rc::clone(&material_right),
-//     });
-
-//     world.add(Rc::clone(&sphere_ground));
-//     world.add(Rc::clone(&sphere_center));
-//     world.add(Rc::clone(&sphere_left));
-//     world.add(Rc::clone(&sphere_bubble));
-//     world.add(Rc::clone(&sphere_right));
-
-//     world
-// }
-
 impl Scene {
     pub fn gen_final_scene() -> Scene {
         let mut objects = HittableList::new();
@@ -347,10 +282,6 @@ impl Scene {
             Arc::new(RotateY::new(Arc::new(boxes2), 15.0)),
             Vec3::new(-100.0, 270.0, 395.0),
         )));
-        // objects.add(Arc::new(Translate::new(
-        //     Arc::new(boxes2),
-        //     Vec3::new(-100.0, 270.0, 395.0),
-        // )));
 
         let camera = Arc::new(PerspectiveCamera::new(
             Vec3::new(478.0, 278.0, -600.0),
